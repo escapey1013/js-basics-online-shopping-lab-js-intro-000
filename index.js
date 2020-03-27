20f47baec8945d1d13055793619a6c5cefedc2cb
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-27 17:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      delete cart.[i];
+      cart.splice(i, i);
     }
   }
   return cart
