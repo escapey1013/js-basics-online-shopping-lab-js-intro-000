b3e9876afeefb7e424fd2a02cad467b6ba2e8484
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-27 16:33:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   } else {
     var message = "In your cart, you have"
-    for (let i = 0; i < cart.length - 1; i++) {
+    for (let i = 0; i < (cart.length - 1); i++) {
       var newMessage = ` ${getCart()[i].itemName} at $${getCard()[i].price},`
       var message = newMessage;
     }
